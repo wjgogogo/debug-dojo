ba892636173c3554692a5523488ffca9dab338e2
Wenjun Xu <906626481@qq.com>
2023-03-08 09:14:41 +0800

    feat: 项目初始化

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <div>Source Map Test</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  devtool: "nosources-cheap-module-source-map",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: "./dist",
+    port: 8000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              [
+                "@babel/preset-react",
+                {
+                  runtime: "automatic",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: "body",
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+};
